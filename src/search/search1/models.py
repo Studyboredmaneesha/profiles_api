1a8df77cd14eccf58f94caf754db1e3955819762
Maneesha mohan <maneesha@example.com>
2020-10-02 21:21:37 +0530

    Added migrtn.

--- src/search/search1/models.py
+++ src/search/search1/models.py
@@ -1,6 +1,4 @@
 from django.db import models
-
-# Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=50)
 
@@ -10,9 +8,9 @@ class Category(models.Model):
 class Food(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(decimal_places=2, max_digits=4)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    category = models.ForeignKey(category, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
 
-        
+# Create your models here.
