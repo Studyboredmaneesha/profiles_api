54929fb54315af8b7b667b20934c7841e775d540
Maneesha mohan <maneesha@example.com>
2020-10-03 14:14:55 +0530

    commit.

--- src/search/search1/urls.py
+++ src/search/search1/urls.py
@@ -1,6 +1,10 @@
 
-from django.conf.urls import url
+from django.urls import path
+
+
+from . import views
 
 urlpatterns = [
-   url('foods/', views.FoodAPIView.as_view()),
+   path('foods/', views.FoodAPIView.as_view()),
+
 ]
