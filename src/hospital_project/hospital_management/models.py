f5eb01f72c7b8e8cfffa672cc847a53f8ec58ad8
Maneesha mohan <maneesha@example.com>
2020-10-03 16:18:27 +0530

    copmmit.

--- src/hospital_project/hospital_management/models.py
+++ src/hospital_project/hospital_management/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Doctor(models.Model):
+    name = models.CharField(max_length=50)
+    department = models.CharField(max_length=100)
+    telephone = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+class Patient(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    age = models.IntegerField()
+    telephone = models.IntegerField()
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
